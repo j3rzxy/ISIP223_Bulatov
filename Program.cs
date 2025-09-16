e0da47e70098fe03a8fd220afc50bf5d5d8c945a
j3rzxy <j3rzxy@DESKTOP-BH9V1PR>
2025-09-16 13:36:58 +0300

    Добавлены начальные шаблоны функций и полей класса

--- Program.cs
+++ Program.cs
@@ -1,1 +1,41 @@
-﻿
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+class Store
+{
+    public int ID;
+    public string Name = "Product";
+    public int Price;
+    public int Quantity;
+    public enum Availability
+    {
+        None, // 0
+        One, // 1
+        MoreThanOne //2+
+    };
+
+    public void Add()
+    {
+
+    }
+
+    public void Delete()
+    {
+
+    }
+
+    public void Order()
+    {
+
+    }
+
+    public void Sell()
+    {
+
+    }
+
+    public void Research()
+    {
+
+    }
+}
