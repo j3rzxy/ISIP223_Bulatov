404167bda9772cc08fbf9637c5c4cd3eae488afb
234998 <234998@KB37-118-C13>
2025-10-08 14:00:01 +0300

    Созданы все неигровые персонажи в виде классов.

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.ConstrainedExecution;
-
+//Игрок
 public class Player
 {
     public int HP { get; set; }
@@ -42,59 +42,131 @@ public class Player
 }
 public abstract class Enemy
 {
-    public int HP { get; set; }
+    public double HP { get; set; }
     public double Attack { get; set; }
     public double Defense { get; set; }
 
-    public Enemy(int hp, double attack, double defense)
+    public Enemy(double hp, double attack, double defense)
     {
         HP = hp;
         Attack = attack;
         Defense = defense;
     }
 }
-
+//Гоблины
 public class Goblin : Enemy
 {
     public double CritDamage { get; set; }
-    public Goblin(int hp, double attack, double defense, double critdamage)
+    public Goblin(double hp, double attack, double defense, double critdamage)
         : base(hp, attack, defense) 
     {
-        HP = 80;
+        HP = 80.0;
         Attack = 40;
         Defense = 20;
         CritDamage = 1.3;
     }
-    public void Ability()
+    public virtual void Critical()
     {
         Attack *= CritDamage; 
     }
 }
-
+public class ВВГ : Goblin
+{
+    public ВВГ(double hp, double attack, double defense, double critdamage)
+        : base(hp, attack, defense, critdamage)
+    {
+        HP *= 2;
+        Attack *= 1.5;
+        Defense *= 1.2;
+        CritDamage *= 1.1;
+    }
+    public override void Critical()
+    {
+        base.Critical();
+    }
+}
+//Скелеты
 public class Skeleton : Enemy
 {
-    public Skeleton(int hp, double attack, double defense)
+    public Skeleton(double hp, double attack, double defense)
         : base(hp, attack, defense)
     {
-        HP = 60;
+        HP = 60.0;
         Attack = 30;
         Defense = 15;
     }
-    public void Ability()
+    public virtual void IgnoreDef()
     {
         
     }
 }
-
-public class Wizard
+public class Ковальский : Skeleton
 {
+    public Ковальский(double hp, double attack, double defense)
+        : base(hp, attack, defense)
+    {
+        HP *= 2.5;
+        Attack *= 1.3;
+        Defense *= 1.4;
+    }
+    public override void IgnoreDef()
+    {
+        base.IgnoreDef();
+    }
+}
+//Маги
+public class Wizard : Enemy
+{
+    public double FreezeChance { get; set; }
+    public Wizard(double hp, double attack, double defense, double freezeChance)
+        : base(hp, attack, defense)
+    {
+        HP = 70.0;
+        Attack = 50;
+        Defense = 0;
+        FreezeChance = 50;
+    }
+    public virtual void Freeze() 
+    { 
+        
+    }
 
 }
+public class АрхимагCPP : Wizard
+{
+    public АрхимагCPP(double hp, double attack, double defense, double freezeChance)
+        : base(hp, attack, defense, freezeChance)
+    {
+        HP *= 1.8;
+        Attack *= 1.6;
+        Defense *= 1.1;
+        FreezeChance *= 1.1;
+    }
+    public override void Freeze()
+    {
+        base.Freeze();
+    }
+}
+public class ПестовCMM : Wizard
+{
+    public ПестовCMM(double hp, double attack, double defense, double freezeChance)
+        : base(hp, attack, defense,  freezeChance)
+    {
+        HP *= 1.3;
+        Attack *= 1.8;
+        Defense *= 0.6;
+    }
+    public override void Freeze()
+    {
+        base.Freeze();
+    }
+}
+//Предметы
 public class Chest
 {
     public string HealPotion { get; set; }
 }
-
+//Основной код игры
 public class Game
 {
 
