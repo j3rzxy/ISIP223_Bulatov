3996f01f2f59fd8bd21d97bba7a716abdec02dd0
234998 <234998@KB37-118-C13>
2025-10-08 11:08:44 +0300

    Создана начальная версия функции хода.

--- Program.cs
+++ Program.cs
@@ -2,7 +2,42 @@
 using System.Collections.Generic;
 using System.Linq;
 
-public abstract class Entity
+public class Player
+{
+    public string HP { get; set; }
+    public string Equipment {  get; set; }
+    public string Weapon {  get; set; }
+
+    public Player(string hp, string equipment, string weapon)
+    {
+        HP = hp;
+        Equipment = equipment;
+        Weapon = weapon;
+    }
+
+    public void PlayerStep()
+    {
+        Console.WriteLine($"Выберите стратегию хода:");
+        Console.WriteLine("1. Атака");
+        Console.WriteLine("2. Защита");
+        if (!int.TryParse(Console.ReadLine(), out int StepChoice) || StepChoice != 1 || StepChoice != 2)
+        {
+            Console.WriteLine("Ошибка: введите либо 1, либо 2 для выбора стратегии.");
+            Console.ReadKey();
+            return;
+        }
+        if (StepChoice == 1) 
+        {
+            Console.WriteLine("-- АТАКА! --");
+
+        }
+        if (StepChoice == 2)
+        {
+            Console.WriteLine("-- ЗАЩИТА! --");
+
+        }
+}
+public class Enemy
 {
 
 }
