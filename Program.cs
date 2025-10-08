902cba16e3276ef16c79bdb5454b426415b554da
234998 <234998@KB37-118-C13>
2025-10-08 13:26:06 +0300

    Добавлены классы всех врагов кроме боссов.

--- Program.cs
+++ Program.cs
@@ -1,16 +1,18 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.ConstrainedExecution;
 
 public class Player
 {
-    public string HP { get; set; }
-    public string Equipment {  get; set; }
-    public string Weapon {  get; set; }
+    public int HP { get; set; }
+    public string Equipment { get; set; }
+    public string Weapon { get; set; }
 
-    public Player(string hp, string equipment, string weapon)
+    public Player(int hp, string equipment, string weapon)
     {
-        HP = hp;
+        HP = 100;
         Equipment = equipment;
         Weapon = weapon;
     }
@@ -26,7 +28,7 @@ public class Player
             Console.ReadKey();
             return;
         }
-        if (StepChoice == 1) 
+        if (StepChoice == 1)
         {
             Console.WriteLine("-- АТАКА! --");
 
@@ -36,8 +38,64 @@ public class Player
             Console.WriteLine("-- ЗАЩИТА! --");
 
         }
+    }
 }
-public class Enemy
+public abstract class Enemy
+{
+    public int HP { get; set; }
+    public double Attack { get; set; }
+    public double Defense { get; set; }
+
+    public Enemy(int hp, double attack, double defense)
+    {
+        HP = hp;
+        Attack = attack;
+        Defense = defense;
+    }
+}
+
+public class Goblin : Enemy
+{
+    public double CritDamage { get; set; }
+    public Goblin(int hp, double attack, double defense, double critdamage)
+        : base(hp, attack, defense) 
+    {
+        HP = 80;
+        Attack = 40;
+        Defense = 20;
+        CritDamage = 1.3;
+    }
+    public void Ability()
+    {
+        Attack *= CritDamage; 
+    }
+}
+
+public class Skeleton : Enemy
+{
+    public Skeleton(int hp, double attack, double defense)
+        : base(hp, attack, defense)
+    {
+        HP = 60;
+        Attack = 30;
+        Defense = 15;
+    }
+    public void Ability()
+    {
+        
+    }
+}
+
+public class Wizard
+{
+
+}
+public class Chest
+{
+    public string HealPotion { get; set; }
+}
+
+public class Game
 {
 
 }
