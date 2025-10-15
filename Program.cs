f0206b186c61048490c61f3200b9efe7a1e4d85a
234998 <234998@KB37-118-C13>
2025-10-15 14:04:21 +0300

    Базовый шаблон.

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+
+class Program 
+{
+    public static void Main()
+    {
+
+    }
+}
